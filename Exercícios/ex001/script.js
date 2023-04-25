b3b698ff9ee86af706624b8127a9ab5855d870de
Danilo P. de Souza <121466843+DevDaniloSouza@users.noreply.github.com>
2023-04-25 11:42:55 -0300

    ex001 Concluído

--- Exercícios/ex001/script.js
+++ Exercícios/ex001/script.js
@@ -1,17 +1,12 @@
-let n1 = document.getElementById('iname1')
-let v1 = document.getElementById('ivel1')
-let n2 = document.getElementById('iname2')
-let v2 = document.getElementById('ivel2')
+const n1 = prompt('Informe o nome do primeiro veículo:')
+const v1 = prompt('Informe a velocidade do primeiro veículo:')
+const n2 = prompt('Informe o nome do segundo veículo:')
+const v2 = prompt('Informe a velocidade do segundo veículo:')
 
-let vel1 = Number(v1)
-let vel2 = Number(v2)
-
-function Verificar() {
-    if (vel1.value > vel2.value) {
-        res.innerHTML = `<p>O carro ${n1} é o carro mais rápido</p>`
-    } if else (vel2.value > vel1.value) {
-        res.innerHTML = `<p>O carro ${n2} é o carro mais rápido</p>`
-    } else {
-        res.innerHTML = `<p>Os carros possuem a mesma velocidade</p>`
-    }
+if (v1 > v2) {
+    alert(n1 + "é mais rápido que" + n2)
+} else if (v2 > v1) {
+    alert(n2 + 'é mais rápido que' + n1)
+} else {
+    alert(n1 + 'e' + n2 + 'possuem a mesma velocidade')
 }
