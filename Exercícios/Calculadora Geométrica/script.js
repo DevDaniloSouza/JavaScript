39fc51b81a64b799ea8e6505c4e771eb58b3721f
Danilo P. de Souza <121466843+DevDaniloSouza@users.noreply.github.com>
2023-05-02 17:33:36 -0300

    Exercício Corrigido
    Embora não estivesse errado... Hehe

--- Exercícios/Calculadora Geométrica/script.js
+++ Exercícios/Calculadora Geométrica/script.js
@@ -11,30 +11,50 @@ do {
         '\n6. Sair'
     )
 
+    function calcSquare() {
+        let side = prompt('Informe o valor do lado do quadrado:')
+        alert('A área do quadrado vale: ' + side*side)
+    }
+
+    function calcTri() {
+        let baseT = prompt('Informe o valor da base do triângulo:')
+        let heightT = prompt('Informe o valor da altura do triângulo:')
+        alert('A área do triângulo vale: ' + baseT*heightT/2)
+    }
+
+    function calcRec() {
+        let baseRec = prompt('Informe o valor da base do retângulo:')
+        let heightRec = prompt('Informe o valor da altura do retângulo:')
+        alert('A área do retângulo vale: ' + baseRec*heightRec)
+    }
+
+    function calcTrap() {
+        let Base = parseFloat(prompt('Informe o valor da base maior do trapézio:'))
+        let base = parseFloat(prompt('Informe o valor da base menor do trapézio:'))
+        let height = parseFloat(prompt('Informe o valor da altura do trapézio:'))
+        alert('A área do trapézio vale: ' + (Base + base) * height / 2)
+    }
+
+    function calcCircle() {
+        let ray = parseFloat(prompt('Informe o valor do raio do círculo:'))
+        alert('A área do círculo vale (considerando pi "3,14"): ' + 3.14 * (ray*ray))
+    }
+
     switch (option) {
         case '1':
-            let side = prompt('Informe o valor do lado do quadrado:')
-            alert('A área do quadrado vale: ' + side*side)
+            calcSquare()
             break
         case '2':
-            let baseSq = prompt('Informe o valor da base do triângulo:')
-            let heightSq = prompt('Informe o valor da altura do triângulo:')
-            alert('A área do triângulo vale: ' + baseSq*heightSq/2)
+            calcTri()
             break
         case '3':
-            let baseRec = prompt('Informe o valor da base do retângulo:')
-            let heightRec = prompt('Informe o valor da altura do retângulo:')
-            alert('A área do retângulo vale: ' + baseRec*heightRec)
+            calcRec()
             break
         case '4':
-            let Base = parseFloat(prompt('Informe o valor da base maior do trapézio:'))
-            let base = parseFloat(prompt('Informe o valor da base menor do trapézio:'))
-            let height = parseFloat(prompt('Informe o valor da altura do trapézio:'))
-            alert('A área do trapézio vale: ' + (Base + base) * height / 2)
+            calcTrap()
             break
         case '5':
-            let ray = parseFloat(prompt('Informe o valor do raio do círculo:'))
-            alert('A área do círculo vale (considerando pi "3,14"): ' + 3.14 * (ray*ray))
+            calcCircle()
             break
         case '6':
             alert('Encerrando...')
