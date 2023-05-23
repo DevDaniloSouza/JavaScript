a273a34c78af74131a26d9921f1c628520cdef4d
Danilo P. de Souza <121466843+DevDaniloSouza@users.noreply.github.com>
2023-05-23 15:29:54 -0300

    Calculadora finalizada

--- Módulo 2/Calc.js/script.js
+++ Módulo 2/Calc.js/script.js
@@ -1,5 +1,5 @@
 const main = document.querySelector('main')
-const root = document.querySelectorAll(':root')
+const root = document.querySelector(':root')
 const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
 
@@ -36,6 +36,37 @@ input.addEventListener("keydown", function(ev) {
 document.getElementById('equal').addEventListener('click', calculate)
 
 function calculate() {
+    resultInput.classList.add('error')
+    resultInput.value = 'ERROR'
     const result = eval(input.value)
     resultInput.value = result
+    resultInput.classList.remove('error')
 }
+
+document.getElementById('copyToClipboard').addEventListener('click', function(ev) {
+    const button = ev.currentTarget
+    if (button.innerText === 'Copy') {
+        button.innerText = 'Copied'
+        button.classList.add('success')
+        window.navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = 'Copy'
+        button.classList.remove('success')
+    }
+})
+
+document.getElementById('theme').addEventListener('click', function() {
+    if (main.dataset.theme === 'dark') {
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91')
+        main.dataset.theme = 'dark'
+    }
+})
